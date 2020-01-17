33b723a43e6f7869d8bad39bb426bccc6ecc2a19
ICDeadPpl <jan@biffstek.se>
2020-01-17 09:29:15 +0100

    Fixed/added comments.
    Removed appdata source check, as it wasn't really necessary.

--- backup-appdata-usb-vm.sh
+++ backup-appdata-usb-vm.sh
@@ -1,44 +1,50 @@
 #!/bin/bash
 
+# Author: Jan Karjalainen <jan@biffstek.se>
+# Github repository: https://github.com/ICDeadPpl/UnraidScripts
 # This script makes backups of all the folders in Unraid's Docker appdata folder.
 # All folders are backed up in separate tar.gz files in the dated destination directory.
-# Backup folders older than
+# Backup folders older than BACKUP_DAYS are deleted.
 # It also backs up the USB drive and the libvirt.img file.
 
+# Do not edit these two lines.
 source /boot/config/docker.cfg
 source /boot/config/domain.cfg
-BACKUP_DEST_APP="/mnt/user/CommunityApplicationsAppdataBackup"
-BACKUP_DEST_USB="/mnt/user/Community_Applications_USB_Backup"
-BACKUP_DEST_VM="/mnt/user/Community_Applications_VM_XML_Backup"
-BACKUP_DAYS=14
-SKIP_DIRECTORY=temp,plex # No spaces in directory names!
 
-if [ -w  "$DOCKER_APP_CONFIG_PATH" ]; then
-    cd "$DOCKER_APP_CONFIG_PATH"
-    echo "Stopping Docker service."
-    /etc/rc.d/rc.docker stop
-    if [ -w  "$BACKUP_DEST_APP" ]; then
-        mkdir -p "$BACKUP_DEST_APP/$(date +%Y-%m-%d)"
-        ls | while read d
-         do
-		    # Test for skipped directories.						
-			for i in ${SKIP_DIRECTORY//,/ }
-			do
-				# Skip directories
-				if [ "$d" == "$i" ]; then
-					echo Skipping directory "$i".
-					continue 2
-				fi
-			done
-            echo Backing up $d directory.
-            tar czf "$BACKUP_DEST_APP/$(date +%Y-%m-%d)/$d-$(date +%Y-%m-%d).tar.gz" "$d"
-         done
+# Edit these lines to your own liking.
+BACKUP_DEST_APP="/mnt/user/CommunityApplicationsAppdataBackup" # Appdata backup destination.
+BACKUP_DEST_USB="/mnt/user/Community_Applications_USB_Backup" # Flash drive destination.
+BACKUP_DEST_VM="/mnt/user/Community_Applications_VM_XML_Backup" # libvirt.img destination.
+BACKUP_DAYS=14 # Delete backups older than this amount of days.
+SKIP_DIRECTORY=temp,plex # Skip these appdata directories. No spaces in directory names!
+
+cd "$DOCKER_APP_CONFIG_PATH"
+echo "Stopping Docker service."
+/etc/rc.d/rc.docker stop
+
+# Check that backup destination is mounted & writable.
+if [ -w  "$BACKUP_DEST_APP" ]; then
+	mkdir -p "$BACKUP_DEST_APP/$(date +%Y-%m-%d)"
+	ls | while read d
+	 do
+		# Test for skipped directories.
+		for i in ${SKIP_DIRECTORY//,/ }
+		do
+			if [ "$d" == "$i" ]; then
+				echo Skipping directory "$i".
+				continue 2
+			fi
+		done
+        # Backup directory into tar.gz file.
+		echo Backing up $d directory.
+		tar czf "$BACKUP_DEST_APP/$(date +%Y-%m-%d)/$d-$(date +%Y-%m-%d).tar.gz" "$d"
+	 done
     echo "Deleting backups older than $BACKUP_DAYS days."
     find "$BACKUP_DEST_APP"/* -type d -ctime +"$BACKUP_DAYS" | xargs rm -rf
-    fi
-    echo "Start Docker service."
-    /etc/rc.d/rc.docker start
 fi
+
+echo "Start Docker service."
+/etc/rc.d/rc.docker start
 
 echo "Backing up USB drive."
 rsync -a /boot/ "$BACKUP_DEST_USB/"
