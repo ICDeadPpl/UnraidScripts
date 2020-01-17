08c96a155bb2e0905f1c5f97e5747c2e69794e0f
ICDeadPpl <jan@biffstek.se>
2020-01-17 09:10:48 +0100

    Changed skipped directories and deleting older backups functionality.

--- backup-appdata-usb-vm.sh
+++ backup-appdata-usb-vm.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# Author: Jan Karjalainen <jan@biffstek.se>
-# Github repository: https://github.com/ICDeadPpl/UnraidScripts
 # This script makes backups of all the folders in Unraid's Docker appdata folder.
 # All folders are backed up in separate tar.gz files in the dated destination directory.
-# Backup folders older than BACKUP_DAYS are deleted.
+# Backup folders older than
 # It also backs up the USB drive and the libvirt.img file.
 
-# Do not edit.
 source /boot/config/docker.cfg
 source /boot/config/domain.cfg
-
-# Edit these 4 lines to your own liking.
 BACKUP_DEST_APP="/mnt/user/CommunityApplicationsAppdataBackup"
 BACKUP_DEST_USB="/mnt/user/Community_Applications_USB_Backup"
 BACKUP_DEST_VM="/mnt/user/Community_Applications_VM_XML_Backup"
 BACKUP_DAYS=14
+SKIP_DIRECTORY=temp,plex # No spaces in directory names!
 
 if [ -w  "$DOCKER_APP_CONFIG_PATH" ]; then
     cd "$DOCKER_APP_CONFIG_PATH"
@@ -25,15 +21,20 @@ if [ -w  "$DOCKER_APP_CONFIG_PATH" ]; then
         mkdir -p "$BACKUP_DEST_APP/$(date +%Y-%m-%d)"
         ls | while read d
          do
-		    # Add directories you want to exclude on the next line.
-            if [ "$d" == 'temp' ] || [ "$d" == 'plex' ]; then
-                continue
-            fi
+		    # Test for skipped directories.						
+			for i in ${SKIP_DIRECTORY//,/ }
+			do
+				# Skip directories
+				if [ "$d" == "$i" ]; then
+					echo Skipping directory "$i".
+					continue 2
+				fi
+			done
             echo Backing up $d directory.
             tar czf "$BACKUP_DEST_APP/$(date +%Y-%m-%d)/$d-$(date +%Y-%m-%d).tar.gz" "$d"
          done
     echo "Deleting backups older than $BACKUP_DAYS days."
-    find "$BACKUP_DEST_APP"/* -type d -ctime +$BACKUP_DAYS -exec rm -rf {} \;
+    find "$BACKUP_DEST_APP"/* -type d -ctime +"$BACKUP_DAYS" | xargs rm -rf
     fi
     echo "Start Docker service."
     /etc/rc.d/rc.docker start
